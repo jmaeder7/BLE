40b86ae55dd9e1553d89d62e4dcc944853a7840a
unknown <jacobmaeder@gmail.com>
2023-03-26 10:59:01 -0500

    removing build folders so they are not part of the commit process

--- bootloader-apploader/GNU ARM v10.3.1 - Default/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := C:/Users/Maeder/SimplicityStudio/SDKs/gecko_sdk//protocol/bluetooth/lib/EFR32BG22/GCC/libapploader.a C:/Users/Maeder/SimplicityStudio/SDKs/gecko_sdk//platform/radio/rail_lib/autogen/librail_release/librail_config_bgm220pc22hna_gcc.a C:/Users/Maeder/SimplicityStudio/SDKs/gecko_sdk//platform/radio/rail_lib/autogen/librail_release/librail_module_efr32xg22_gcc_release.a
-
-LIBS := -lgcc -lc -lm -lnosys
-
