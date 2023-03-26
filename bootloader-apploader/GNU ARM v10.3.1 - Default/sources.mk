d1a9a36b94b527e34df37df1a1a31295074b801a
unknown <jacobmaeder@gmail.com>
2023-03-26 09:48:57 -0500

    adding initial code

---
+++ bootloader-apploader/GNU ARM v10.3.1 - Default/sources.mk
@@ -0,0 +1,43 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+S79_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S79_UPPER_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+EXECUTABLES := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+autogen \
+gecko_sdk_4.2.1/hardware/board/src \
+gecko_sdk_4.2.1/platform/Device/SiliconLabs/BGM22/Source \
+gecko_sdk_4.2.1/platform/bootloader/communication/apploader \
+gecko_sdk_4.2.1/platform/bootloader/core \
+gecko_sdk_4.2.1/platform/bootloader/core/flash \
+gecko_sdk_4.2.1/platform/bootloader/debug \
+gecko_sdk_4.2.1/platform/bootloader/driver \
+gecko_sdk_4.2.1/platform/bootloader/parser/gbl \
+gecko_sdk_4.2.1/platform/bootloader/security \
+gecko_sdk_4.2.1/platform/bootloader/security/ecc \
+gecko_sdk_4.2.1/platform/bootloader/security/sha \
+gecko_sdk_4.2.1/platform/common/src \
+gecko_sdk_4.2.1/platform/common/toolchain/src \
+gecko_sdk_4.2.1/platform/emlib/src \
+gecko_sdk_4.2.1/platform/radio/rail_lib/plugin/fem_util \
+gecko_sdk_4.2.1/platform/radio/rail_lib/plugin/pa-conversions \
+gecko_sdk_4.2.1/platform/security/sl_component/se_manager/src \
+gecko_sdk_4.2.1/platform/security/sl_component/sl_cryptoacc_library/src \
+gecko_sdk_4.2.1/platform/security/sl_component/sl_mbedtls_support/src \
+gecko_sdk_4.2.1/platform/security/sl_component/sl_psa_driver/src \
+gecko_sdk_4.2.1/platform/service/device_init/src \
+gecko_sdk_4.2.1/util/silicon_labs/silabs_core/memory_manager \
+gecko_sdk_4.2.1/util/third_party/mbedtls/library \
+
